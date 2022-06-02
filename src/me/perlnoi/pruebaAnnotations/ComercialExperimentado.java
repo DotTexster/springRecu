6418eb9597435fe1a5f6e59c221176786a8f68ec
DotTexster <ltexocotitla215@gmail.com>
2022-06-02 02:07:57 -0500

    SNSV21 readyrun scope

--- src/me/perlnoi/pruebaAnnotations/ComercialExperimentado.java
+++ src/me/perlnoi/pruebaAnnotations/ComercialExperimentado.java
@@ -2,9 +2,11 @@ package me.perlnoi.pruebaAnnotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("ComercialExperimentado")
+@Scope("prototype")
 public class ComercialExperimentado implements Empleados {
 
 	public ComercialExperimentado() {
