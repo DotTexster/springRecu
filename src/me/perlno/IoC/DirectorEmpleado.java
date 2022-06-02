dac68dd0b87abed85afd75b5cda67c3444d14c87
DotTexster <ltexocotitla215@gmail.com>
2022-06-01 20:29:22 -0500

    Changed value of return

---
+++ src/me/perlno/IoC/DirectorEmpleado.java
@@ -0,0 +1,11 @@
+package me.perlno.IoC;
+
+public class DirectorEmpleado implements Empleados {
+
+	@Override
+	public String getTareas() {
+		// TODO Auto-generated method stub
+		return "Gestionar la plantilla de la empresa";
+	}
+
+}
