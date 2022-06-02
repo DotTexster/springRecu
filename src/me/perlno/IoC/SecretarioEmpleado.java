e28f96a6cff8ba2ee7388a0672b581bc7b76cc8b
DotTexster <ltexocotitla215@gmail.com>
2022-06-01 20:26:26 -0500

    Add value to SecretarioEmpleado

---
+++ src/me/perlno/IoC/SecretarioEmpleado.java
@@ -0,0 +1,11 @@
+package me.perlno.IoC;
+
+public class SecretarioEmpleado implements Empleados {
+
+	@Override
+	public String getTareas() {
+		// TODO Auto-generated method stub
+		return "Gestionar la agenda de los Jefes";
+	}
+
+}
