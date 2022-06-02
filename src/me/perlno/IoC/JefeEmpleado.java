491e7428a46a692332b3f19d7c176b3475d31a15
DotTexster <ltexocotitla215@gmail.com>
2022-06-01 20:16:53 -0500

    SNSV07 JefeEmplado & UsoEmpleado first

---
+++ src/me/perlno/IoC/JefeEmpleado.java
@@ -0,0 +1,8 @@
+package me.perlno.IoC;
+
+public class JefeEmpleado {
+
+	public String getTareas() {
+		return "Gestiono las cuestiones relativas a mis empleados de seccion";
+	}
+}
---
+++ src/me/perlno/IoC/UsoEmpleados.java
@@ -0,0 +1,14 @@
+package me.perlno.IoC;
+
+public class UsoEmpleados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JefeEmpleado empleado = new JefeEmpleado();
+		
+		
+		System.out.println(empleado.getTareas());
+		
+	}
+}
