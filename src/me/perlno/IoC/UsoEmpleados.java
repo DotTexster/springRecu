0a56720c0fed30e906c16448b0a897499c8d6685
DotTexster <ltexocotitla215@gmail.com>
2022-06-01 20:41:00 -0500

    Use of IoC

--- src/me/perlno/IoC/UsoEmpleados.java
+++ src/me/perlno/IoC/UsoEmpleados.java
@@ -1,14 +1,21 @@
 package me.perlno.IoC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoEmpleados {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Empleados Juan = contexto.getBean("miEmpleado", Empleados.class);
+		System.out.println(Juan.getTareas());
+		contexto.close();
+		/*
 		Empleados empleado = new DirectorEmpleado();
 		
 		
 		System.out.println(empleado.getTareas());
-		
+		*/
 	}
 }
