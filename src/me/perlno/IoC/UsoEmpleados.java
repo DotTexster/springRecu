1da7a334795b06030dad0813b29e43d12188003f
DotTexster <ltexocotitla215@gmail.com>
2022-06-01 20:28:53 -0500

    Autogenerate DirectorEmpleado

--- src/me/perlno/IoC/UsoEmpleados.java
+++ src/me/perlno/IoC/UsoEmpleados.java
@@ -5,7 +5,7 @@ public class UsoEmpleados {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Empleados empleado = new SecretarioEmpleado();
+		Empleados empleado = new DirectorEmpleado();
 		
 		
 		System.out.println(empleado.getTareas());
