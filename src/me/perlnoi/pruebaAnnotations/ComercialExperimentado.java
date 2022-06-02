19dae0282d8937ef29cb0dc550e5d9450c97dcf8
DotTexster <ltexocotitla215@gmail.com>
2022-06-02 01:27:45 -0500

    SNSV18 autowired

--- src/me/perlnoi/pruebaAnnotations/ComercialExperimentado.java
+++ src/me/perlnoi/pruebaAnnotations/ComercialExperimentado.java
@@ -1,9 +1,15 @@
 package me.perlnoi.pruebaAnnotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("ComercialExperimentado")
 public class ComercialExperimentado implements Empleados {
+
+	@Autowired
+	public ComercialExperimentado(CreacionInformeFinanciero nuevoInforme) {
+		this.nuevoInforme = nuevoInforme;
+	}
 
 	@Override
 	public String getTareas() {
@@ -14,7 +20,11 @@ public class ComercialExperimentado implements Empleados {
 	@Override
 	public String getInforme() {
 		// TODO Auto-generated method stub
-		return "Informe generado por el comercial";
+		// return "Informe generado por el comercial";
+		return nuevoInforme.getInformeFinanciero();
 	}
-
+	
+	private CreacionInformeFinanciero nuevoInforme;
+	
+	
 }
---
+++ src/me/perlnoi/pruebaAnnotations/CreacionInformeFinanciero.java
@@ -0,0 +1,7 @@
+package me.perlnoi.pruebaAnnotations;
+
+public interface CreacionInformeFinanciero {
+	
+	public String getInformeFinanciero();
+	
+}
